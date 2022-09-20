9c48f0d48653cd3c9aabb8f3f24dfbd6dbf0553c
= <=>
2022-09-20 10:47:18 +0700

    oke

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    titleWeb: "Hi Crush",
+    introTitle: 'My love',
+    introDesc: `
+    Trái đất đã vốn lạ thường..................................................                                                                         
+    Mà sao em cứ mãi đi nhầm đường................................
+    Lạc vào tim anh lẻ loi..........................................................
+    Đằng sau chữ yêu chắc hẳn là thương.........................`,
+    btnIntro: '^hiuhiu^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰 ',
+    desc: 'Phải chăng em là của anh :)) ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'why you love me, why??? <3',
+    btnReply: 'Send for me <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Tui biết mà 🥰. Yêu Cậu nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.youtube.com/watch?v=r7GtxXYEvgU'
+}
